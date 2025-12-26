7afdeef74b1c65bbbcdde6e8422d9e75d3a53ade
Rahul2271 <rraahhuullch@gmail.com>
2025-12-26 15:18:13 +0530

    d

--- components/header.tsx
+++ components/header.tsx
@@ -24,6 +24,7 @@ export default function Header() {
       <nav className="ml-auto flex gap-4 sm:gap-6 items-center">
         <button
           onClick={() => router.push("/blogs")}
+          // onClick={() => setShowWelcomeModal(true)}
           className="text-sm font-medium hover:text-primary transition-colors"
         >
           Blogs
